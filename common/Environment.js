4d71e9fe823de059d2561a62ca73b070ea6da717
manojharpalani <manojharpalani@gmail.com>
2019-02-26 22:20:13 -0800

    Update Environment.js

--- common/Environment.js
+++ common/Environment.js
@@ -1,6 +1,6 @@
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDqzUpxJUs8n4pm-d8FEwKh9hEVGP8OyWk",
+  apiKey: "placeholder",
   authDomain: "foodatgate-77f82.firebaseapp.com",
   databaseURL: "https://foodatgate-77f82.firebaseio.com",
   projectId: "foodatgate-77f82",
@@ -9,7 +9,7 @@ const firebaseConfig = {
 };
 
 const Environment = {
-  googleApiKey: 'AIzaSyClGlMUq7VVpOltAzd3RgPBON1RuLMdVHM',
+  googleApiKey: 'placeholder',
   logLevel: "DEBUG", //RAW, DEBUG, INFO
   firebaseConfig: firebaseConfig,
   sentryDNS: 'https://4f59b4e78142429886745f75fd469769:51efd4f8edbc486da0330e5b2804e138@sentry.io/181859'
